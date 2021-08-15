808285ab10fc43408c0a6b653103faf1047fde7a
AaA <67450869+Awara-Amin@users.noreply.github.com>
2021-08-15 15:59:40 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+// alert("hi kaka");
+var navBar = document.querySelector(".nav-1");
+var bar = document.querySelector(".bar-icon");
+var links = document.querySelector("ul");
+
+bar.addEventListener("click", () => {
+  // alert("hi");
+    links.classList.toggle("notShown");
+
+
+})
